b474a56d6b22e5165e0d9853be2ff8470e98b6b7
Sarah Melanie White <sawrrawr@gmail.com>
2022-02-23 10:55:37 -0330

    inital commit after creating Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
+import { tsPropertySignature } from "@babel/types";
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   const eventHandler = props.action;
+ 
+   return (
+   <button 
+      onClick={props.onClick} 
+      className={buttonClass}
+      disabled={props.disabled}>
+      {props.children}
+   </button>
+   );
 }
