18e63af25b7e55436f554c55dc8a4d8c373e63b4
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-05 17:09:07 -0330

    add form testing

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   //props: student(string), interviewers(Array), interviewer(number), onSave, onCancel
   const [student, setStudent] = useState(props.student || '');
+  const [error, setError] = useState("");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const reset = () => {
     setStudent('');
@@ -15,48 +16,35 @@ const cancel = () => {
   props.onCancel()
 }
 
-const save = () => {
-  console.log(`save function taking student state: `, student)
-  console.log(`save func taking interviewer: `, interviewer)
-  props.onSave(student, interviewer)
+// const save = () => {
+//   // console.log(`save function taking student state: `, student)
+//   // console.log(`save func taking interviewer: `, interviewer)
+//   props.onSave(student, interviewer)
+// }
+
+const validate = () => {
+  if (student === "") {
+    setError("Student name cannot be blank");
+    return;
+  }
+
+  props.onSave(student, interviewer);
 }
 
-// function save(name, interviewer) {
-//   const interview = {
-//     student: name,
-//     interviewer
-//   };
-//   props.bookInterview(id, interviewer)
-//   transition(SHOW)
-// }
-
-// function bookInterview(id, interview) {
-//   console.log(id, interview);
-//   const appointment = {
-//     ...state.appointments[id],
-//     interview: { ...interview }
-//   };
-//   const appointments = {
-//     ...state.appointments,
-//     [id]: appointment
-//   };
-//   setState({
-//     ...state,
-//     appointments
-//   });
-// }
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
+            data-testid="student-name-input"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList 
         interviewers={props.interviewers}
@@ -67,7 +55,7 @@ const save = () => {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
